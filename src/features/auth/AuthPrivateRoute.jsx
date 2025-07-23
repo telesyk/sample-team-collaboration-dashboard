b32d20094c74fcdc8630e566936a7a3309995afb
telesyk <telesyk89@gmail.com>
2025-07-23 12:21:25 +0200

    Update AuthPrivateRoute to use Navigate component.

--- src/features/auth/AuthPrivateRoute.jsx
+++ src/features/auth/AuthPrivateRoute.jsx
@@ -1,15 +1,9 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router'
+import { Navigate } from 'react-router'
 import { useAuth } from '@/context'
 import { PATHS } from '@/constants'
 
 export default function AuthPrivateRoute({ children }) {
   const { user } = useAuth()
-  const navigate = useNavigate()
 
-  useEffect(() => {
-    if (!user) navigate(PATHS.auth)
-  }, [])
-
-  return children
+  return !!user ? children : <Navigate to={PATHS.auth} />
 }
