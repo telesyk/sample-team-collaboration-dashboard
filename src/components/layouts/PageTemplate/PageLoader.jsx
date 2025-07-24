51938fe01b79c09096d3d9b7d21961c29f489e30
telesyk <telesyk89@gmail.com>
2025-07-24 12:34:40 +0200

    Update UI of PageLoader

--- src/components/layouts/PageTemplate/PageLoader.jsx
+++ src/components/layouts/PageTemplate/PageLoader.jsx
@@ -1,10 +1,17 @@
 export default function PageLoader() {
-  console.debug('is Loading page')
-  // need to extend component to show skeleeton of the page
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-base-100">
-      <span className="loading loading-spinner loading-lg text-primary mb-4"></span>
-      <span className="text-lg text-base-content">Loading...</span>
+    <div className="flex min-h-screen flex-col gap-4 md:gap-8 p-8">
+      <div className="skeleton h-14 w-full"></div>
+      <div className="flex flex-col gap-4 md:gap-8 flex-auto">
+        <div className="skeleton h-4 w-1/3"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="flex gap-4 md:gap-8 flex-auto">
+          <div className="skeleton w-full md:w-auto md:basis-1/2"></div>
+          <div className="hidden md:block skeleton md:w-auto md:basis-1/2"></div>
+        </div>
+      </div>
+      <div className="skeleton h-14 w-full"></div>
     </div>
   )
 }
