582638a273c6b085ccc86ca76af60a65fe0b2c5f
telesyk <telesyk89@gmail.com>
2025-07-27 15:18:05 +0200

    Use reducer to management the auth state. Update AuthContext. Extend constants with ACTIONs.

--- src/features/auth/AuthContext.jsx
+++ src/features/auth/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect, useContext } from 'react'
+import { createContext, useReducer, useEffect, useContext } from 'react'
 import {
   onAuthStateChanged,
   signInWithEmailAndPassword,
@@ -7,74 +7,91 @@ import {
   signInWithPopup,
 } from 'firebase/auth'
 import { auth, googleProvider } from '@/utils'
+import { ACTION } from '@/constants'
+import authReducer from './authReducer.js'
 
 export const AuthContext = createContext(null)
 
+const initialState = {
+  user: null,
+  error: null,
+  isLoading: true,
+}
+
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null)
-  const [errorMessage, setError] = useState(null)
-  const [isLoading, setLoading] = useState(true)
+  const [state, dispatch] = useReducer(authReducer, initialState)
 
   useEffect(() => {
     return onAuthStateChanged(auth, user => {
-      setUser(user)
-      setLoading(false) // <-- Set loading to false only after auth state is resolved
+      dispatch({ type: ACTION.SET_USER, payload: user })
     })
   }, [])
 
   const login = async (email, password) => {
+    dispatch({ type: ACTION.SET_LOADING, payload: true })
     try {
       await signInWithEmailAndPassword(auth, email, password)
+      dispatch({ type: ACTION.CLEAR_ERROR })
     } catch (error) {
-      setError(prev => ({
-        ...prev,
-        message: error.message,
-        code: error.code,
-      }))
+      dispatch({
+        type: ACTION.SET_ERROR,
+        payload: { message: error.message, code: error.code },
+      })
       console.error(error)
+    } finally {
+      dispatch({ type: ACTION.SET_LOADING, payload: false })
     }
   }
 
   const loginWithGoogle = async () => {
+    dispatch({ type: ACTION.SET_LOADING, payload: true })
     try {
       await signInWithPopup(auth, googleProvider)
+      dispatch({ type: ACTION.CLEAR_ERROR })
     } catch (error) {
-      setError(prev => ({
-        ...prev,
-        message: error.message,
-        code: error.code,
-      }))
+      dispatch({
+        type: ACTION.SET_ERROR,
+        payload: { message: error.message, code: error.code },
+      })
       console.error(error)
+    } finally {
+      dispatch({ type: ACTION.SET_LOADING, payload: false })
     }
   }
 
   const signup = async (email, password) => {
+    dispatch({ type: 'SET_LOADING', payload: true })
     try {
       await createUserWithEmailAndPassword(auth, email, password)
+      dispatch({ type: 'CLEAR_ERROR' })
     } catch (error) {
+      dispatch({
+        type: 'SET_ERROR',
+        payload: { message: error.message, code: error.code },
+      })
       console.error(error)
-      setError(prev => ({
-        ...prev,
-        message: error.message,
-        code: error.code,
-      }))
+    } finally {
+      dispatch({ type: 'SET_LOADING', payload: false })
     }
   }
 
   const logout = async () => {
+    dispatch({ type: 'SET_LOADING', payload: true })
     await signOut(auth)
+    dispatch({ type: 'SET_USER', payload: null })
+    dispatch({ type: 'SET_LOADING', payload: false })
   }
 
   return (
     <AuthContext.Provider
       value={{
-        user,
+        user: state.user,
         login,
         loginWithGoogle,
         logout,
         signup,
-        error: errorMessage,
-        isLoading,
+        error: state.error,
+        isLoading: state.isLoading,
       }}
     >
       {children}
